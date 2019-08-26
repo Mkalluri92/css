90bae619366ea2e82fb725feb45aa8f143823dbb
Mounika Kalluri <mkalluri92@gmail.com>
2019-08-26 16:41:55 -0700

    creating a weather report for a city using zipcode

---
+++ script.js
@@ -0,0 +1,49 @@
+ $("#sub"). on("click", function(){
+     //console.log("Step 2: Button Click");
+     var id=$("#place").val();
+     if (isNaN(id)){
+         alert("Enter valid zipcode.");
+         return;
+     }
+     if(id==""){
+         alert("Zip Code can't be empty.");
+         return;
+     }
+     //console.log("Step 3: Making Ajax Call");
+     $.ajax(
+        {
+        url: 'http://api.openweathermap.org/data/2.5/weather?zip='+id+',us&APPID=512ee1e8d76d7ad1f6e81acafcc685b0',
+        type: 'GET',
+        success: function(data){
+            //console.log(data);
+            showImage(data.weather[0].main);
+            showLocation(data);
+            
+        },
+        error: function(data){
+            //console.log("Step 4: Got the Error");
+            alert("The Entered ZipCode doesnot exist");
+            
+        }
+    }
+    )
+})
+
+function showImage(imageData){
+    $("#weatherImage").attr("src",weatherImages[imageData]);
+}
+
+var weatherImages={
+    "Clear": "./images/clearsky.png",
+    "Rain": "./images/rain.png",
+    "Snow": "./images/snow.png",
+    "Clouds": "./images/clouds.png"
+}
+
+function showLocation(data){
+    locationHtml='<span> In '+data.name+' the weather now is '+data.weather[0].main+'</span><br />'+
+    '<span> Temperature is: '+data.main.temp+'</span><br />'+
+    '<span>Max Temperature is: '+data.main.temp_max+'</span><br />'+
+    '<span>Min Temperature is: '+data.main.temp_min+'</span<<br />'
+    $("#location").html(locationHtml);
+}
